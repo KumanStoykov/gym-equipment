473507795a2e76a4017298573e176fd1fef2e67f
Kuman Stoykov <kumanstoykov@gmail.com>
2022-11-04 22:17:21 +0100

    Create user payload password extract

---
+++ server/utils/userPayload.js
@@ -0,0 +1,10 @@
+module.userPayload = (user) => {
+    return {
+        _id: user._id,
+        email: user.email,
+        firstName: user.firstName | '',
+        lastName: user.lastName | '',
+        phone: user.phone | '',
+        address: user.address | '',
+    };
+};
