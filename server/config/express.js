9a82ec805ae38dcb466b034086c550e8053574d3
Kuman Stoykov <kumanstoykov@gmail.com>
2022-11-05 19:45:45 +0100

    Create config file

---
+++ server/config/express.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const cors = require('cors');
+const path = require('path');
+
+const config = require('./index');
+const router = require('./router');
+
+module.exports = (app) => {
+
+    app.use(express.static(path.join(__dirname, '..', '..', 'client', 'build')));
+
+    app.use(cors(config.CORS));
+
+    app.use(express.json());
+
+    app.use(cookieParser());
+
+    app.use(router);
+}
