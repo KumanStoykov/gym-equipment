e76fe8971de80fd1be2be3698a12c49cb6f703ee
Kuman Stoykov <kumanstoykov@gmail.com>
2022-11-06 14:41:30 +0100

    Create formidable utility

---
+++ server/utils/formidableParsePromise.js
@@ -0,0 +1,11 @@
+module.exports = (req, form) => {
+    return new Promise((resolve, reject) => {
+        form.parse(req, (err, fields, files) => {
+            if (err) {
+                reject(err);
+            } else {
+                resolve([fields, files]);
+            }
+        });
+    });
+}
