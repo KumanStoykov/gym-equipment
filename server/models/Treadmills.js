af2dccb3d2857ee2a88a14c8aa6f081afb8cc26e
Kuman Stoykov <kumanstoykov@gmail.com>
2022-11-06 12:38:00 +0100

    Create model Treadmills

---
+++ server/models/Treadmills.js
@@ -0,0 +1,70 @@
+const mongoose = require('mongoose');
+
+
+const treadMillsSchema = mongoose.Schema({
+
+    brand: {
+        type: String,
+        require: true
+    },
+    price: {
+        type: Number,
+        require: true
+    },
+    promoPrice: {
+        type: Number,
+        require: true
+    },
+    madeIn: {
+        type: String,
+        require: true
+    },
+    dimensions: {
+        type: String,
+        require: true
+    },
+    equipmentWeight: {
+        type: Number,
+        require: true
+    },
+    runningSurface: {
+        type: String,
+        require: true
+    },
+    motorPower: {
+        type: String,
+        require: true
+    },
+    minSpeed: {
+        type: Number,
+        require: true
+    },
+    maxSpeed: {
+        type: Number,
+        require: true
+    },
+    InclineMin: {
+        type: Number,
+        require: true
+    },
+    InclineMax: {
+        type: Number,
+        require: true
+    },
+    availableLanguages: {
+        type: String,
+        require: true
+    },
+    description: {
+        type: String,
+        require: true
+    }
+
+});
+
+treadMillsSchema.set('timestamps', true);
+
+const TreadMills = mongoose.model('TreadMills', treadMillsSchema);
+
+module.exports = TreadMills;
+
