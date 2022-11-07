f8097f5dee79ce46572d6210aa5347fd3dbef695
Kuman Stoykov <kumanstoykov@gmail.com>
2022-11-07 17:19:50 +0100

    Added getById and deleteBench

--- server/services/benchService.js
+++ server/services/benchService.js
@@ -2,6 +2,10 @@ const Bench = require('../models/Bench');
 
 exports.getAll = (page, sort) => Bench.find({}).sort({ createdAt: sort }).skip(page * 9).limit(9);
 
+exports.getById = (benchId) => Bench.findById(benchId);
+
 exports.create = (benchData) => Bench.create(benchData);
+
+exports.deleteBench = (benchId) => Bench.findByIdAndDelete(benchId);
 
 exports.count = () => Bench.countDocuments({});
