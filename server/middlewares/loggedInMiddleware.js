292482c09e3d567e0a6847b8e41ffdefb9b6d9dc
Kuman Stoykov <kumanstoykov@gmail.com>
2022-11-08 16:35:57 +0100

    Create middleware loggedIn

---
+++ server/middlewares/loggedInMiddleware.js
@@ -0,0 +1,20 @@
+const { jwtVerify, createToken } = require('../utils/jwtUtil');
+
+const { COOKIE_TOKEN_NAME, SECRET } = require('../config');
+
+
+module.exports = () => async (req, res, next) => {
+    const token = req.cookies[COOKIE_TOKEN_NAME];
+
+    try {
+        const user = await jwtVerify(token, SECRET);
+        res.cookies(COOKIE_TOKEN_NAME, createToken(user), { httpOnly: true });
+
+        req.user = user;
+        next();
+
+    } catch(err) {
+        res.clearCookie(COOKIE_TOKEN_NAME);
+        res.status(401).send({ message: 'Please log in'});
+    }
+}
