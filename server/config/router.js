fe1c27145e05252ba237cf3bdd2ac263d1272d39
Kuman Stoykov <kumanstoykov@gmail.com>
2022-11-08 16:29:00 +0100

    Added bench, bike, dumbbell, rack, treadmill controllers

--- server/config/router.js
+++ server/config/router.js
@@ -1,7 +1,17 @@
 const router = require('express').Router();
 
 const userController = require('../controllers/userController');
+const benchController = require('../controllers/benchController');
+const bikeController = require('../controllers/bikeController');
+const dumbbellController = require('../controllers/dumbbellController');
+const rackController = require('../controllers/rackController');
+const treadmillsController = require('../controllers/treadmillsController');
 
 router.use('/user', userController);
+router.use('/bench', benchController);
+router.use('/bike', bikeController);
+router.use('/dumbbell', dumbbellController);
+router.use('/rack', rackController);
+router.use('/treadmill', treadmillsController);
 
 module.exports = router;
