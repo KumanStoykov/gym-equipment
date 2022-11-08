0e87bdd4e50d4896ee135136c0307401bb9200dc
Kuman Stoykov <kumanstoykov@gmail.com>
2022-11-08 16:42:52 +0100

    Create rack service

---
+++ server/services/rackService.js
@@ -0,0 +1,11 @@
+const Rack = require('../models/Rack');
+
+exports.getAll = (page, sort) => Rack.find({}).sort({ createdAt: sort }).skip(page * 9).limit(9);
+
+exports.getById = (rackId) => Rack.findById(rackId);
+
+exports.create = (rackData) => Rack.create(rackData);
+
+exports.deleteRack = (rackId) => Rack.findByIdAndDelete(rackId);
+
+exports.count = () => Rack.countDocuments({});
