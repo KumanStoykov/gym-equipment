fed73a306ec33a238acd65368d6869814ea3465e
Kuman Stoykov <kumanstoykov@gmail.com>
2022-11-08 16:35:33 +0100

    Create middleware check credential

---
+++ server/middlewares/checkCredentialMiddleware.js
@@ -0,0 +1,16 @@
+const { jwtVerify } = require('../utils/jwtUtil');
+
+const { COOKIE_TOKEN_NAME, SECRET } = require('../config');
+
+module.exports = () => async (req, res, next) => {
+    const token = req.cookies[COOKIE_TOKEN_NAME];
+
+    try {
+        const user = await jwtVerify(token, SECRET);
+        req.user = user;
+
+    } catch(err) {
+        res.user = undefined;
+        next();
+    }
+}
