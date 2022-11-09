34bae4dbcd39663756bf757c440ce03d2dfc4baf
Kuman Stoykov <kumanstoykov@gmail.com>
2022-11-09 14:10:15 +0100

    Create filter-price component

---
+++ client/src/app/shared/filter-price/filter-price.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FilterPriceComponent } from './filter-price.component';
+
+describe('FilterPriceComponent', () => {
+  let component: FilterPriceComponent;
+  let fixture: ComponentFixture<FilterPriceComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ FilterPriceComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(FilterPriceComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ client/src/app/shared/filter-price/filter-price.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-filter-price',
+  templateUrl: './filter-price.component.html',
+  styleUrls: ['./filter-price.component.scss']
+})
+export class FilterPriceComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
