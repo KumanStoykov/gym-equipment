1e399db4bcdae874a63f25c28ed3cadbde566e61
Kuman Stoykov <kumanstoykov@gmail.com>
2022-11-09 14:14:14 +0100

    Update path

--- client/src/app/app-routing.module.ts
+++ client/src/app/app-routing.module.ts
@@ -1,10 +1,33 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './core/home/home.component';
+
+const routes: Routes = [
+    {
+        path: '',
+        pathMatch: 'full',
+        component: HomeComponent
+    },
+    {
+        path: 'treadmills',
+        loadChildren: () => import('./treadmill/treadmill.module').then(m => m.TreadmillModule)
+    },
+    {
+        path: 'auth',
+        loadChildren: () => import('./auth/auth.module').then(m => m.AuthModule)
+    },
+    {
+        path: '**',
+        redirectTo: '/'
+    }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+    imports: [RouterModule.forRoot(routes, {
+        preloadingStrategy: PreloadAllModules,
+        scrollPositionRestoration: 'enabled'
+    })],
+    exports: [RouterModule]
 })
 export class AppRoutingModule { }
