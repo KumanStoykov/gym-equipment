bdfa88c2442423779063ed8f4ca082ce0a80d563
Kuman Stoykov <kumanstoykov@gmail.com>
2022-11-09 14:10:34 +0100

    Create filter-order component

---
+++ client/src/app/shared/filter-order/filter-order.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-filter-order',
+  templateUrl: './filter-order.component.html',
+  styleUrls: ['./filter-order.component.scss']
+})
+export class FilterOrderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
