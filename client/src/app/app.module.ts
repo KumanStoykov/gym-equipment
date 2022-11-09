96f835faf5c892a17d8661aef171ce2454cbeb3f
Kuman Stoykov <kumanstoykov@gmail.com>
2022-11-09 14:13:58 +0100

    Added CoreModule and SharedModule

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -3,6 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AuthModule } from './auth/auth.module';
+import { BenchModule } from './bench/bench.module';
+import { BikeModule } from './bike/bike.module';
+import { CoreModule } from './core/core.module';
+import { DumbbellModule } from './dumbbell/dumbbell.module';
+import { RackModule } from './rack/rack.module';
+import { SharedModule } from './shared/shared.module';
+import { TreadmillModule } from './treadmill/treadmill.module';
 
 @NgModule({
   declarations: [
@@ -10,6 +18,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    CoreModule,
+    SharedModule,
     AppRoutingModule
   ],
   providers: [],
