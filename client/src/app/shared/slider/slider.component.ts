47a857c3232203e9b314d90283529af501ca3afe
Kuman Stoykov <kumanstoykov@gmail.com>
2022-11-10 14:15:37 +0100

    Create slider component

---
+++ client/src/app/shared/slider/slider.component.ts
@@ -0,0 +1,46 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+import { faAngleLeft, faAngleRight } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+    selector: 'app-slider',
+    templateUrl: './slider.component.html',
+    styleUrls: ['./slider.component.scss']
+})
+export class SliderComponent implements OnInit {
+
+    @Input() images!: string[];
+
+    imgIndex = 0;
+
+    icons = {
+        faAngleLeft,
+        faAngleRight
+    }
+
+
+    constructor() { }
+
+    ngOnInit(): void {
+    }
+
+    public indexNext(): void {
+        if (this.imgIndex === this.images.length - 1) {
+            this.imgIndex = 0;
+        } else {
+            this.imgIndex += 1;
+        }
+    }
+
+    public indexPreview(): void {
+        if (this.imgIndex === 0) {
+            this.imgIndex = this.images.length - 1;
+        } else {
+            this.imgIndex -= 1;
+        }
+    }
+
+    public imageClick(currentIndex: number): void {
+        this.imgIndex = currentIndex;
+    }
+}
