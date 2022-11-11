7fcb5696dbe9775c7fc00f98069376e04170b257
Kuman Stoykov <kumanstoykov@gmail.com>
2022-11-11 14:38:11 +0100

    Create dumbbell-info

---
+++ client/src/app/dumbbell/dumbbell-info/dumbbell-info.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dumbbell-info',
+  templateUrl: './dumbbell-info.component.html',
+  styleUrls: ['./dumbbell-info.component.scss']
+})
+export class DumbbellInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
