774ebd3081e2a8dbf47df871a780e23286010f8f
Kuman Stoykov <kumanstoykov@gmail.com>
2022-11-11 14:39:50 +0100

    Create bike-info

---
+++ client/src/app/bike/bike-info/bike-info.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-bike-info',
+  templateUrl: './bike-info.component.html',
+  styleUrls: ['./bike-info.component.scss']
+})
+export class BikeInfoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
