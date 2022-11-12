35508702fe85b741e88f7e1b4d1c864e35c7c8a1
Kuman Stoykov <kumanstoykov@gmail.com>
2022-11-12 15:28:27 +0100

    Added bench-details, list and info component

--- client/src/app/bench/bench.module.ts
+++ client/src/app/bench/bench.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { BenchListComponent } from './bench-list/bench-list.component';
+import { BenchInfoComponent } from './bench-info/bench-info.component';
+import { BenchDetailsComponent } from './bench-details/bench-details.component';
+import { SharedModule } from '../shared/shared.module';
+import { BenchRoutingModule } from './bench-routing.module';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    BenchListComponent,
+    BenchInfoComponent,
+    BenchDetailsComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule,
+    BenchRoutingModule
   ]
 })
 export class BenchModule { }
