caaee3ca55145c41c22ac646ed5f4c46e79366c4
Kuman Stoykov <kumanstoykov@gmail.com>
2022-11-12 15:19:37 +0100

    Added html and sccs

--- client/src/app/rack/rack-list/rack-list.component.ts
+++ client/src/app/rack/rack-list/rack-list.component.ts
@@ -7,6 +7,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RackListComponent implements OnInit {
 
+    heroTitle = 'RACKS';
+    pageTitle = 'SUPREME RUNNING PERFORMANCE';
+
   constructor() { }
 
   ngOnInit(): void {
