358895c49e811fd734473267d9787ed5474d95f6
Kuman Stoykov <kumanstoykov@gmail.com>
2022-11-12 15:18:45 +0100

    Update with rack-details, info and list components

--- client/src/app/rack/rack.module.ts
+++ client/src/app/rack/rack.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RackDetailsComponent } from './rack-details/rack-details.component';
+import { RackInfoComponent } from './rack-info/rack-info.component';
+import { RackListComponent } from './rack-list/rack-list.component';
+import { SharedModule } from '../shared/shared.module';
+import { RackRoutingModule } from './rack-routing.module';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    RackDetailsComponent,
+    RackInfoComponent,
+    RackListComponent,
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule,
+    RackRoutingModule
   ]
 })
 export class RackModule { }
