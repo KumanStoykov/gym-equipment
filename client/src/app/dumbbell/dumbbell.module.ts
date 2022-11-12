00b7dbf4d49b7842c9448b31d34f3c80539bc623
Kuman Stoykov <kumanstoykov@gmail.com>
2022-11-12 15:22:05 +0100

    Update with dumbbell-details, info and list components

--- client/src/app/dumbbell/dumbbell.module.ts
+++ client/src/app/dumbbell/dumbbell.module.ts
@@ -1,12 +1,23 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { DumbbellInfoComponent } from './dumbbell-info/dumbbell-info.component';
+import { DumbbellListComponent } from './dumbbell-list/dumbbell-list.component';
+import { DumbbellDetailsComponent } from './dumbbell-details/dumbbell-details.component';
+import { SharedModule } from '../shared/shared.module';
+import { DumbbellRoutingModule } from './dumbbell-routing.module';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    DumbbellInfoComponent,
+    DumbbellListComponent,
+    DumbbellDetailsComponent
+  ],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule,
+    DumbbellRoutingModule
   ]
 })
 export class DumbbellModule { }
