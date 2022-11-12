ce3a31f43b7621f51fc94d1ddcc8fce5b261fddd
Kuman Stoykov <kumanstoykov@gmail.com>
2022-11-12 15:20:20 +0100

    Added html and scss

--- client/src/app/rack/rack-details/rack-details.component.ts
+++ client/src/app/rack/rack-details/rack-details.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RackDetailsComponent implements OnInit {
 
+    heroTitle = 'DETAILS';
+
   constructor() { }
 
   ngOnInit(): void {
