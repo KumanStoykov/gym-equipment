5b6ddb23b37c65e7831aa7f23d2dd9776d672dd0
Kuman Stoykov <kumanstoykov@gmail.com>
2022-11-12 15:15:36 +0100

    Create routing module

---
+++ client/src/app/dumbbell/dumbbell-routing.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { DumbbellDetailsComponent } from './dumbbell-details/dumbbell-details.component';
+import { DumbbellListComponent } from './dumbbell-list/dumbbell-list.component';
+
+
+const routes: Routes = [
+  {
+    path: 'dumbbell',
+    children: [
+        {
+            path: '',
+            pathMatch: 'full',
+            component: DumbbellListComponent
+        },
+        {
+            path: 'details',
+            component: DumbbellDetailsComponent
+        }
+    ]
+  }
+];
+
+@NgModule({
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule]
+})
+export class DumbbellRoutingModule { }
