862ecf1deb43f916e4b7a3e076ebd9baf89db676
Kuman Stoykov <kumanstoykov@gmail.com>
2022-11-12 15:26:52 +0100

    Changed variable name

--- client/src/app/core/header/header.component.ts
+++ client/src/app/core/header/header.component.ts
@@ -8,8 +8,8 @@ import { Component, OnInit } from '@angular/core';
 export class HeaderComponent implements OnInit {
 
     menuIsToggle: boolean = false;
-    shopIsToggle: boolean = false;
-    loginIsToggle: boolean = false;
+    strengthIsToggle: boolean = false;
+    accountIsToggle: boolean = false;
 
     constructor() { }
 
@@ -18,25 +18,25 @@ export class HeaderComponent implements OnInit {
 
     public menuToggle(): void {
         this.menuIsToggle = !this.menuIsToggle;
-        this.shopIsToggle = false;
-        this.loginIsToggle = false;
+        this.strengthIsToggle = false;
+        this.accountIsToggle = false;
     }
 
     public handleOutsideClick(): void {
         this.menuIsToggle = false;
-        this.shopIsToggle = false;
-        this.loginIsToggle = false;
+        this.strengthIsToggle = false;
+        this.accountIsToggle = false;
     }
 
     public dropdownToggle(btnName: string): void {
-        console.log(btnName)
-        if (btnName === 'login') {
-            this.loginIsToggle = !this.loginIsToggle;
-            this.shopIsToggle = false;
+
+        if (btnName === 'account') {
+            this.accountIsToggle = !this.accountIsToggle;
+            this.strengthIsToggle = false;
 
         } else {
-            this.shopIsToggle = !this.shopIsToggle;
-            this.loginIsToggle = false;
+            this.strengthIsToggle = !this.strengthIsToggle;
+            this.accountIsToggle = false;
         }
     }
 
