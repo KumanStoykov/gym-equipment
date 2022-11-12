36c568522b1358c8ab48a8f6f9915f56569a0174
Kuman Stoykov <kumanstoykov@gmail.com>
2022-11-12 15:24:45 +0100

    Update html and scss

--- client/src/app/dumbbell/dumbbell-details/dumbbell-details.component.ts
+++ client/src/app/dumbbell/dumbbell-details/dumbbell-details.component.ts
@@ -7,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DumbbellDetailsComponent implements OnInit {
 
+    heroTitle = 'DETAILS';
+
   constructor() { }
 
   ngOnInit(): void {
