ac64fd724c7ef9170459fe33d5f7be8f0994af5e
Kuman Stoykov <kumanstoykov@gmail.com>
2022-11-14 21:21:39 +0100

    Create email validator

---
+++ client/src/app/shared/validators/email.ts
@@ -0,0 +1,16 @@
+import { AbstractControl, AsyncValidatorFn, ValidationErrors } from '@angular/forms';
+import { Observable, of } from 'rxjs';
+import { delay, map } from 'rxjs/operators';
+
+
+export function emailValidator(): AsyncValidatorFn {
+    return (control: AbstractControl): Observable<ValidationErrors | null> => {
+        return of({}).pipe(
+            delay(1000),
+            map(() => {
+                const isMatch = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/.test(control.value);
+                return isMatch ? null : { invalidEmail: true }
+            })
+        );
+    };
+}
