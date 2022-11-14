1f9e4f806442e346eafa2ff0d9a0e8332f5fea25
Kuman Stoykov <kumanstoykov@gmail.com>
2022-11-14 21:22:54 +0100

    Create minLength validator

---
+++ client/src/app/shared/validators/minLength.ts
@@ -0,0 +1,16 @@
+import { AbstractControl, AsyncValidatorFn, ValidationErrors } from '@angular/forms';
+import { Observable, of } from 'rxjs';
+import { delay, map } from 'rxjs/operators';
+
+
+export function minLengthValidator(numLength: number): AsyncValidatorFn {
+    return (control: AbstractControl): Observable<ValidationErrors | null> => {
+        return of({}).pipe(
+            delay(1000),
+            map(() => {
+                const valueLength =  control.value.length;
+                return valueLength >= numLength ? null : { minLength: true }
+            })
+        );
+    };
+}
