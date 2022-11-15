49d0b169301dc56b93f51a25a19ae35f2d0cb420
Kuman Stoykov <kumanstoykov@gmail.com>
2022-11-15 14:45:02 +0100

    Changed query

--- server/services/userService.js
+++ server/services/userService.js
@@ -3,6 +3,6 @@ const User = require('../models/User');
 
 exports.getById = (userId) => User.findById(userId);
 
-exports.getByEmail = (email) => User.find({ email: email });
+exports.getByEmail = (email) => User.findOne({ email });
 
 exports.createUser = (user) => User.create(user);
