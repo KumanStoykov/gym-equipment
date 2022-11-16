54812f0d779eb1ec52c3ff5ba2c990e1950d63de
Kuman Stoykov <kumanstoykov@gmail.com>
2022-11-16 21:28:44 +0100

    Implement onSubmit method

--- client/src/app/auth/login/login.component.ts
+++ client/src/app/auth/login/login.component.ts
@@ -1,18 +1,65 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { auth_success } from 'src/app/+store/actions';
+import { emailValidator, minLengthValidator } from 'src/app/shared/validators';
+import { AuthService } from '../auth.service';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent {
 
     heroTitle = 'ACCOUNT';
     pageTitle = 'LOGIN';
 
-  constructor() { }
+    loginForm!: FormGroup;
+    error: string = '';
+    isLoading: boolean = false;
+    registerForm: any;
 
-  ngOnInit(): void {
-  }
+    constructor(
+        private fb: FormBuilder,
+        private authService: AuthService,
+        private store: Store,
+        private router: Router
+    ) {
+        this.loginForm = this.fb.group({
+            email: ['', [Validators.required], emailValidator()],
+            password: ['', [Validators.required], minLengthValidator(6)]
+        });
+    }
 
+    onSubmit(): void {
+
+        if (this.loginForm.invalid || this.loginForm.pending) { return; }
+
+        const email = this.loginForm.value.email;
+        const password = this.loginForm.value.password;
+
+        this.isLoading = true;
+
+        this.authService.login({ email, password }).subscribe({
+            next: user => {
+                this.store.dispatch(auth_success({
+                    _id: user._id,
+                    email: user.email,
+                    firstName: user.firstName || '',
+                    lastName: user.lastName || '',
+                    phone: user.phone || '',
+                    address: user.address || '',
+                    isAdmin: user.isAdmin
+                }));
+                this.isLoading = false;
+                this.loginForm.reset();
+                this.router.navigate(['/']);
+            },
+            error: (err) => {
+                console.log(err);
+            }
+        })
+    }
 }
