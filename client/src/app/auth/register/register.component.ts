e09745072522070270f1915affc1eec7e83a5491
Kuman Stoykov <kumanstoykov@gmail.com>
2022-11-16 21:30:22 +0100

    Update onSubmit

--- client/src/app/auth/register/register.component.ts
+++ client/src/app/auth/register/register.component.ts
@@ -2,9 +2,13 @@ import { Component, OnDestroy } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { Subject } from 'rxjs';
-import { AuthService } from '../auth.service';
+import { Store } from '@ngrx/store';
 
+
+import { AuthService } from '../auth.service';
 import { emailValidator, minLengthValidator, equalValueAsFactory } from '../../shared/validators';
+import { IAuthState } from 'src/app/+store/reducers';
+import { auth_success } from 'src/app/+store/actions';
 
 @Component({
     selector: 'app-register',
@@ -26,10 +30,11 @@ export class RegisterComponent implements OnDestroy {
         private fb: FormBuilder,
         private authService: AuthService,
         private router: Router,
+        private store: Store<IAuthState>
     ) {
 
         this.registerForm = this.fb.group({
-            email: ['', [Validators.required],  emailValidator()],
+            email: ['', [Validators.required], emailValidator()],
             password: ['', [Validators.required], minLengthValidator(6)],
             repeatPassword: ['', [Validators.required], equalValueAsFactory(
                 () => this.registerForm?.get('password'), this.killSubscription
@@ -49,8 +54,18 @@ export class RegisterComponent implements OnDestroy {
         this.isLoading = true;
 
         this.authService.register({ email, password }).subscribe({
-            next: (user) => {
-                console.log(user)
+            next: user => {
+                this.store.dispatch(auth_success({
+                    _id: user._id,
+                    email: user.email,
+                    firstName: user.firstName || '',
+                    lastName: user.lastName || '',
+                    phone: user.phone || '',
+                    address: user.address || '',
+                    isAdmin: user.isAdmin
+                }));
+                this.isLoading = false;
+                this.registerForm.reset();
                 this.router.navigate(['/']);
             },
             error: (err) => {
