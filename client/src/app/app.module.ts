8ab16cbfccdd887b37bed048a6ef1b94726a32cc
Kuman Stoykov <kumanstoykov@gmail.com>
2022-11-16 21:23:59 +0100

    Added store-devtools

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,20 +7,26 @@ import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
 import { StoreModule } from '@ngrx/store';
 import { HttpClientModule } from '@angular/common/http';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+
+import * as authReducer from './+store/reducers';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    CoreModule,
-    SharedModule,
-    HttpClientModule,
-    AppRoutingModule,
-    StoreModule.forRoot({}, {})
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+    declarations: [
+        AppComponent
+    ],
+    imports: [
+        BrowserModule,
+        CoreModule,
+        SharedModule,
+        HttpClientModule,
+        AppRoutingModule,
+        StoreModule.forRoot({}),
+        StoreModule.forFeature(authReducer.featureKey, authReducer.authReducer),
+        StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production })
+    ],
+    providers: [],
+    bootstrap: [AppComponent]
 })
 export class AppModule { }
