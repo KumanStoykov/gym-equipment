99c99f9c743d1296f8f50fd801d2d49c8ed81744
Kuman Stoykov <kumanstoykov@gmail.com>
2022-11-20 23:06:01 +0100

    Create interface treadmill

---
+++ client/src/app/shared/interfaces/treadmill.ts
@@ -0,0 +1,20 @@
+export interface ITreadmill {
+    _id: string;
+    brand: string;
+    madeIn: string;
+    price: number;
+    promoPrice: number;
+    modeIn: string;
+    material: string;
+    dimensions: string;
+    equipmentWeight: number;
+    runningSurface: number;
+    motorPower: string;
+    minSpeed: number;
+    maxSpeed: number;
+    InclineMin: number;
+    InclineMax: number;
+    availableLanguages: string;
+    description: string;
+    images: string[];
+}
