1432e2e4e33a2d710c9189ea9c770f71f046456b
Kuman Stoykov <kumanstoykov@gmail.com>
2022-11-21 20:27:13 +0100

    Create bikeinterface

---
+++ client/src/app/shared/interfaces/bike.ts
@@ -0,0 +1,20 @@
+export interface IBIke {
+    _id: string;
+    brand: string;
+    price: number;
+    promoPrice: number;
+    madeIn: string;
+    material: string;
+    lengthBike: string;
+    equipmentWeight: number;
+    adjustableLevelingFeet: number;
+    resistanceSystem: string;
+    transportWheels: number;
+    connectivity: number;
+    minUserLength: number;
+    maxUserLength: number;
+    display: string;
+    availableLanguages: string;
+    description: string;
+    images: string[];
+}
