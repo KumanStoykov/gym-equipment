ef05799bd0bfb17a15da5f84250077a66ffb54f1
Kuman Stoykov <kumanstoykov@gmail.com>
2022-11-22 20:56:00 +0100

    Create rack interface

---
+++ client/src/app/shared/interfaces/rack.ts
@@ -0,0 +1,14 @@
+export interface IRack {
+    _id: string;
+    brand: string;
+    price: number;
+    promoPrice: number;
+    madeIn: string;
+    material: string;
+    minUserWeight: number;
+    maxUserWeight: number;
+    dimensions: string;
+    netWeight: number;
+    description: string;
+    images: string[];
+}
