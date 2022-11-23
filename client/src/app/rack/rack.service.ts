e7daa76d075461874d60704d5d97d3a88b8687ff
Kuman Stoykov <kumanstoykov@gmail.com>
2022-11-23 15:48:46 +0100

    Create rackinterface

---
+++ client/src/app/rack/rack.service.ts
@@ -0,0 +1,22 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+
+import { environment } from '../../environments/environment';
+import { IRack } from '../shared/interfaces';
+
+const API_URL = environment.API_URL;
+
+@Injectable({
+    providedIn: 'root'
+})
+export class RackService {
+
+    constructor(
+        private http: HttpClient
+    ) { }
+
+    create(rackData: any): Observable<IRack> {
+        return this.http.post<IRack>(`${API_URL}/treadmill/create`, rackData, { withCredentials: true });
+    }
+}
