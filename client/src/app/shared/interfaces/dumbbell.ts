b48d73720b26f6fdc26cbb4a117a502bb6cfefaf
Kuman Stoykov <kumanstoykov@gmail.com>
2022-11-23 15:48:27 +0100

    Create dumbbell interface

---
+++ client/src/app/shared/interfaces/dumbbell.ts
@@ -0,0 +1,19 @@
+export interface IDumbbell {
+    _id: string;
+    brand: string;
+    price: number;
+    promoPrice: number;
+    madeIn: string;
+    material: string;
+    knurl: string;
+    rangeOfAvailableWeightsFrom: number;
+    rangeOfAvailableWeightsTo: number;
+    rangeOfDumbbellLengthsFrom: number;
+    rangeOfDumbbellLengthsTo: number;
+    handleDiameterFrom: number;
+    handleDiameterTo: number;
+    cylinderDiameterFrom: number;
+    cylinderDiameterTo: number;
+    description: string;
+    images: string[];
+}
