f305b855453025ef411a0038d9fdfb4db1edf9bc
Kuman Stoykov <kumanstoykov@gmail.com>
2022-11-24 15:41:53 +0100

    Create form and styling

--- client/src/app/admin/create-dumbbell/create-dumbbell.component.ts
+++ client/src/app/admin/create-dumbbell/create-dumbbell.component.ts
@@ -1,15 +1,73 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { DumbbellService } from 'src/app/dumbbell/dumbbell.service';
+import { IDumbbell } from 'src/app/shared/interfaces';
 
 @Component({
-  selector: 'app-create-dumbbell',
-  templateUrl: './create-dumbbell.component.html',
-  styleUrls: ['./create-dumbbell.component.scss']
+    selector: 'app-create-dumbbell',
+    templateUrl: './create-dumbbell.component.html',
+    styleUrls: ['./create-dumbbell.component.scss']
 })
 export class CreateDumbbellComponent implements OnInit {
+    @ViewChild('form') form!: NgForm;
 
-  constructor() { }
+    files: [] = [];
+    fileIsChose: boolean = false;
 
-  ngOnInit(): void {
-  }
+    rack: IDumbbell | undefined;
+    error: string | undefined;
+    isLoading: boolean = false;
+    filesCount: number | undefined;
+
+    constructor(
+        private dumbbellService: DumbbellService,
+        private router: Router
+    ) { }
+
+    ngOnInit(): void {
+    }
+
+
+    onFileSelect(event: any): void {
+        const input = event.target;
+        this.files = input.files;
+        this.filesCount = this.files?.length;
+
+        if (this.filesCount && this.filesCount > 0) {
+            this.fileIsChose = true;
+        } else {
+            this.fileIsChose = false;
+        }
+    }
+
+    onSubmit() {
+
+        const formData = new FormData();
+
+        for (const [k, v] of Object.entries(this.form.value)) {
+            formData.append(k, v as string)
+        }
+
+        for (const [k, v] of Object.entries(this.files)) {
+            formData.append('images' + k, v as string)
+        }
+
+        this.isLoading = true;
+
+        this.dumbbellService.create(formData).subscribe({
+            next: data => {
+                this.router.navigateByUrl('/');
+                this.isLoading = false;
+
+            },
+            error: err => {
+                this.error = err.error.message;
+                this.isLoading = false;
+                console.log(err.error)
+            }
+        })
+
+    }
 
 }
--- client/src/app/admin/create-treadmill/create-treadmill.component.ts
+++ client/src/app/admin/create-treadmill/create-treadmill.component.ts
@@ -1,15 +1,74 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import { Router } from '@angular/router';
+import { ITreadmill } from 'src/app/shared/interfaces';
+import { TreadmillService } from 'src/app/treadmill/treadmill.service';
 
 @Component({
-  selector: 'app-create-treadmill',
-  templateUrl: './create-treadmill.component.html',
-  styleUrls: ['./create-treadmill.component.scss']
+    selector: 'app-create-treadmill',
+    templateUrl: './create-treadmill.component.html',
+    styleUrls: ['./create-treadmill.component.scss']
 })
 export class CreateTreadmillComponent implements OnInit {
+    @ViewChild('form') form!: NgForm;
 
-  constructor() { }
+    files: [] = [];
+    fileIsChose: boolean = false;
 
-  ngOnInit(): void {
-  }
+    treadmill: ITreadmill | undefined;
+    error: string | undefined;
+    isLoading: boolean = false;
+    filesCount: number | undefined;
+
+
+    constructor(
+        private treadmillService: TreadmillService,
+        private router: Router
+    ) { }
+
+    ngOnInit(): void {
+
+    }
+
+    onFileSelect(event: any): void {
+        const input = event.target;
+        this.files = input.files;
+        this.filesCount = this.files?.length;
+
+        if(this.filesCount && this.filesCount > 0) {
+            this.fileIsChose = true;
+        } else {
+            this.fileIsChose = false;
+        }
+    }
+
+    onSubmit() {
+
+        const formData = new FormData();
+
+        for(const[k, v] of Object.entries(this.form.value)) {
+            formData.append(k, v as string)
+        }
+
+        for(const[k, v] of Object.entries(this.files)) {
+            formData.append('images' + k, v as string)
+        }
+
+        this.isLoading = true;
+
+        this.treadmillService.create(formData).subscribe({
+            next: data => {
+                this.router.navigateByUrl('/');
+                this.isLoading = false;
+
+            },
+            error: err => {
+                this.error = err.error.message;
+                this.isLoading = false;
+                console.log(err.error)
+            }
+        })
+
+    }
 
 }
