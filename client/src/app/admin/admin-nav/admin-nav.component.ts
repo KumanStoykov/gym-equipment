a0270fe8f04cf041254929ef06ed128f84461b7e
Kuman Stoykov <kumanstoykov@gmail.com>
2022-11-24 15:37:04 +0100

    Added more routes

--- client/src/app/admin/admin-nav/admin-nav.component.ts
+++ client/src/app/admin/admin-nav/admin-nav.component.ts
@@ -1,15 +1,20 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-admin-nav',
-  templateUrl: './admin-nav.component.html',
-  styleUrls: ['./admin-nav.component.scss']
+    selector: 'app-admin-nav',
+    templateUrl: './admin-nav.component.html',
+    styleUrls: ['./admin-nav.component.scss']
 })
 export class AdminNavComponent implements OnInit {
 
-  constructor() { }
+    dropdownIsToggle: boolean = false;
 
-  ngOnInit(): void {
-  }
+    constructor() { }
 
+    ngOnInit(): void {
+    }
+
+    dropdownToggle(): void {
+     this.dropdownIsToggle = !this.dropdownIsToggle;
+    }
 }
