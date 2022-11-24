1834ef70b497c5759c1781b01daf5e0b888f25b7
Kuman Stoykov <kumanstoykov@gmail.com>
2022-11-24 15:37:41 +0100

    Declaration rest components

--- client/src/app/admin/admin.module.ts
+++ client/src/app/admin/admin.module.ts
@@ -1,12 +1,41 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { SharedModule } from '../shared/shared.module';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ChartProductsComponent } from './chart-products/chart-products.component';
+import { AdminRoutingModule } from './admin-routing.module';
+import { StatisticComponent } from './statistic/statistic.component';
+import { AdminNavComponent } from './admin-nav/admin-nav.component';
+import { SalesVolumeComponent } from './sales-volume/sales-volume.component';
+import { CreateStrengthComponent } from './create-strength/create-strength.component';
+import { CreateTreadmillComponent } from './create-treadmill/create-treadmill.component';
+import { CreateBikeComponent } from './create-bike/create-bike.component';
+import { FormsModule } from '@angular/forms';
+import { CreateRackComponent } from './create-rack/create-rack.component';
+import { CreateBenchComponent } from './create-bench/create-bench.component';
+import { CreateDumbbellComponent } from './create-dumbbell/create-dumbbell.component';
 
 
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+    declarations: [
+        DashboardComponent,
+        ChartProductsComponent,
+        StatisticComponent,
+        AdminNavComponent,
+        SalesVolumeComponent,
+        CreateStrengthComponent,
+        CreateTreadmillComponent,
+        CreateBikeComponent,
+        CreateRackComponent,
+        CreateBenchComponent,
+        CreateDumbbellComponent,
+    ],
+    imports: [
+        CommonModule,
+        SharedModule,
+        FormsModule,
+        AdminRoutingModule
+    ]
 })
 export class AdminModule { }
