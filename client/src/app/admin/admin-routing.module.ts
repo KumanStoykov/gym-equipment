090483f0fb7cde9b3507fee93554ae41f1da5bdd
Kuman Stoykov <kumanstoykov@gmail.com>
2022-11-24 15:37:16 +0100

    Added new routes

--- client/src/app/admin/admin-routing.module.ts
+++ client/src/app/admin/admin-routing.module.ts
@@ -1,5 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CreateBenchComponent } from './create-bench/create-bench.component';
+import { CreateBikeComponent } from './create-bike/create-bike.component';
+import { CreateDumbbellComponent } from './create-dumbbell/create-dumbbell.component';
+import { CreateRackComponent } from './create-rack/create-rack.component';
+import { CreateTreadmillComponent } from './create-treadmill/create-treadmill.component';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { StatisticComponent } from './statistic/statistic.component';
 
@@ -18,6 +23,30 @@ const routes: Routes = [
         {
             path: 'statistic',
             component: StatisticComponent
+        },
+        {
+            path: 'create/treadmill',
+            component: CreateTreadmillComponent
+        },
+        {
+            path: 'create/bike',
+            component: CreateBikeComponent
+        },
+        {
+            path: 'create/rack',
+            component: CreateRackComponent
+        },
+        {
+            path: 'create/bench',
+            component: CreateBenchComponent
+        },
+        {
+            path: 'create/dumbbell',
+            component: CreateDumbbellComponent
+        },
+        {
+            path: '**',
+            redirectTo: '/admin/statistic'
         }
 
     ]
