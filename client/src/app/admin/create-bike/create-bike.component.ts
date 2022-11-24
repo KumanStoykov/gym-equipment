4d7c4e572d5186dd102666436cff65826d115cdf
Kuman Stoykov <kumanstoykov@gmail.com>
2022-11-24 15:40:41 +0100

    Created form and styling

--- client/src/app/admin/create-bike/create-bike.component.ts
+++ client/src/app/admin/create-bike/create-bike.component.ts
@@ -1,15 +1,76 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+import { Router } from '@angular/router';
+import { BikeService } from 'src/app/bike/bike.service';
+import { IBIke } from 'src/app/shared/interfaces';
 
 @Component({
-  selector: 'app-create-bike',
-  templateUrl: './create-bike.component.html',
-  styleUrls: ['./create-bike.component.scss']
+    selector: 'app-create-bike',
+    templateUrl: './create-bike.component.html',
+    styleUrls: ['./create-bike.component.scss']
 })
 export class CreateBikeComponent implements OnInit {
 
-  constructor() { }
+    @ViewChild('form') form!: NgForm;
 
-  ngOnInit(): void {
-  }
+    files: [] = [];
+    fileIsChose: boolean = false;
+
+    bike: IBIke | undefined;
+    error: string | undefined;
+    isLoading: boolean = false;
+    filesCount: number | undefined;
+
+
+    constructor(
+        private bikeService: BikeService,
+        private router: Router
+    ) { }
+
+    ngOnInit(): void {
+
+    }
+
+    onFileSelect(event: any): void {
+        const input = event.target;
+        this.files = input.files;
+        this.filesCount = this.files?.length;
+
+        if (this.filesCount && this.filesCount > 0) {
+            this.fileIsChose = true;
+        } else {
+            this.fileIsChose = false;
+        }
+    }
+
+    onSubmit() {
+
+        const formData = new FormData();
+
+        for (const [k, v] of Object.entries(this.form.value)) {
+            formData.append(k, v as string)
+        }
+
+        for (const [k, v] of Object.entries(this.files)) {
+            formData.append('images' + k, v as string)
+        }
+
+        this.isLoading = true;
+
+        this.bikeService.create(formData).subscribe({
+            next: data => {
+                this.router.navigateByUrl('/');
+                this.isLoading = false;
+
+            },
+            error: err => {
+                this.error = err.error.message;
+                this.isLoading = false;
+                console.log(err.error)
+            }
+        })
+
+    }
 
 }
