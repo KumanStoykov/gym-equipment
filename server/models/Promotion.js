9b384fcf11b7f852e19e2bf468eb61468f5708d4
Kuman Stoykov <kumanstoykov@gmail.com>
2022-12-03 21:12:36 +0100

    Create Promotion model

---
+++ server/models/Promotion.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+const enumProducts = ['Treadmill', 'Bike', 'Rack', 'Bench', 'Dumbbell'];
+
+const promotionSchema = mongoose.Schema({
+    productType: {
+        type: String,
+        require: true,
+        enum: enumProducts
+    },
+    product: {
+        type: mongoose.SchemaTypes.ObjectId,
+        refPath: 'productType'
+    }
+});
+
+promotionSchema.set('timestamps', true);
+
+const Promotion = mongoose.model('Promotion', promotionSchema);
+
+module.exports = Promotion;
