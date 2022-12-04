15fe23b9932daf8c0cda22707fcf450ecc77b064
Kuman Stoykov <kumanstoykov@gmail.com>
2022-12-04 14:35:47 +0100

    Added fetching data

--- client/src/app/shared/product-card/product-card.component.ts
+++ client/src/app/shared/product-card/product-card.component.ts
@@ -2,23 +2,29 @@ import { Component, Input, OnInit } from '@angular/core';
 
 import { faStar, faStarHalf } from '@fortawesome/free-solid-svg-icons';
 
+
 @Component({
     selector: 'app-product-card',
     templateUrl: './product-card.component.html',
     styleUrls: ['./product-card.component.scss']
 })
 export class ProductCardComponent implements OnInit {
+    @Input() cardType!: string;
+    @Input() product!: any;
+
+    hasPromo!: boolean
 
     icons = {
         faStar,
         faStarHalf
     }
 
-    @Input() cardType!: string;
 
-    constructor() { }
+    constructor() {
+    }
 
     ngOnInit(): void {
+        this.hasPromo = this.product.promoPrice > 0;
     }
 
 }
