e8a2ee2c1a03a36a303661df932098ff299fd893
Kuman Stoykov <kumanstoykov@gmail.com>
2022-12-04 14:34:05 +0100

    Added fetch data, notification

--- client/src/app/treadmill/treadmill-details/treadmill-details.component.ts
+++ client/src/app/treadmill/treadmill-details/treadmill-details.component.ts
@@ -1,19 +1,47 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ITreadmill } from 'src/app/shared/interfaces';
+import { TreadmillService } from '../treadmill.service';
 
 
 @Component({
-  selector: 'app-treadmill-details',
-  templateUrl: './treadmill-details.component.html',
-  styleUrls: ['./treadmill-details.component.scss']
+    selector: 'app-treadmill-details',
+    templateUrl: './treadmill-details.component.html',
+    styleUrls: ['./treadmill-details.component.scss']
 })
 export class TreadmillDetailsComponent implements OnInit {
 
     heroTitle = 'DETAILS';
 
+    treadmill!: ITreadmill;
+    isLoading: boolean = false;
+    error: string = '';
 
-  constructor() { }
+    id = this.router.url.split('/')[2];
 
-  ngOnInit(): void {
-  }
+    constructor(
+        private treadmillService: TreadmillService,
+        private router: Router
+    ) { }
+
+    ngOnInit(): void {
+        this.isLoading = true;
+        this.treadmillService.getOne(this.id).subscribe({
+            next: treadmill => {
+                this.treadmill = treadmill;
+                this.isLoading = false;
+            },
+            error: err => {
+                this.isLoading = false;
+                this.error = err.error.message || 'Something went wrong, Please try again later.'
+            }
+        })
+
+    }
+
+    onCloseNot(): void {
+        this.error = '';
+    }
+
 
 }
