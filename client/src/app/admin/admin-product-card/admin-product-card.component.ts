dd2d1d6d7ed78aa0685a0411d6b8b358f57f22df
Kuman Stoykov <kumanstoykov@gmail.com>
2022-12-05 20:42:25 +0100

    Create admin product-card

---
+++ client/src/app/admin/admin-product-card/admin-product-card.component.ts
@@ -0,0 +1,28 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-admin-product-card',
+  templateUrl: './admin-product-card.component.html',
+  styleUrls: ['./admin-product-card.component.scss']
+})
+export class AdminProductCardComponent implements OnInit {
+    @Input() product: any;
+    @Input() cardType!: string;
+
+
+
+    strengthTypes: string[] = ['rack', 'bench', 'dumbbell'];
+
+    constructor() {
+    }
+
+    ngOnInit(): void {
+        console.log(this.cardType)
+        this.cardType = this.cardType.toLowerCase();
+
+        if(this.strengthTypes.includes(this.cardType)) {
+            this.cardType = `/strength/${this.cardType}`;
+        }
+    }
+
+}
