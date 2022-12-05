1c032da607e6df4dd3d7732e58386425eedb6a7a
Kuman Stoykov <kumanstoykov@gmail.com>
2022-12-05 20:40:49 +0100

    Create promotion list component

---
+++ client/src/app/promotion/promotion-list/promotion-list.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { PromotionListComponent } from './promotion-list.component';
+
+describe('PromotionListComponent', () => {
+  let component: PromotionListComponent;
+  let fixture: ComponentFixture<PromotionListComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ PromotionListComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(PromotionListComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ client/src/app/promotion/promotion-list/promotion-list.component.ts
@@ -0,0 +1,58 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { switchMap } from 'rxjs';
+import { IPromotion } from 'src/app/shared/interfaces/promotion';
+import { PromotionService } from '../promotion.service';
+
+@Component({
+  selector: 'app-promotion-list',
+  templateUrl: './promotion-list.component.html',
+  styleUrls: ['./promotion-list.component.scss']
+})
+export class PromotionListComponent implements OnInit {
+
+    heroTitle = 'SALES';
+    pageTitle = 'SALE FOR ALL';
+
+    promotions: IPromotion[] = [];
+    page: number = 1;
+    count: number = 0;
+    isLoading: boolean = false;
+    error: string = '';
+
+    constructor(
+        private promotionService: PromotionService,
+        private activationRouter: ActivatedRoute
+    ) { }
+
+    ngOnInit(): void {
+        this.activationRouter.queryParams.pipe(
+            switchMap(params => {
+                let query = '';
+                Object.entries(params).forEach(([k, v]) => {
+                    query += '&' + k + '=' + v;
+                });
+
+                this.page = params['page'];
+                this.isLoading = true;
+
+                return this.promotionService.getAllPromo(query);
+            })
+        ).subscribe({
+            next: data => {
+                this.promotions = data.promotions;
+                this.count = data.promotionsCount;
+                this.isLoading = false;
+            },
+            error: err => {
+                this.isLoading = false;
+                this.error = err.error.message || 'Something went wrong, Please try again later.';
+            }
+        })
+    }
+
+    onCloseNot(): void {
+        this.error = '';
+    }
+
+}
