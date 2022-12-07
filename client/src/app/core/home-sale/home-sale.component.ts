cb223f8afc70c4f68b1d25001242bbc2382a9932
Kuman Stoykov <kumanstoykov@gmail.com>
2022-12-07 10:36:26 +0100

    Added sales path

--- client/src/app/core/home-sale/home-sale.component.ts
+++ client/src/app/core/home-sale/home-sale.component.ts
@@ -1,21 +1,46 @@
 import { Component, OnInit } from '@angular/core';
 
 import { faDumbbell } from '@fortawesome/free-solid-svg-icons';
+import { PromotionService } from 'src/app/promotion/promotion.service';
+import { IPromotion } from 'src/app/shared/interfaces/promotion';
 
 @Component({
-  selector: 'app-home-sale',
-  templateUrl: './home-sale.component.html',
-  styleUrls: ['./home-sale.component.scss']
+    selector: 'app-home-sale',
+    templateUrl: './home-sale.component.html',
+    styleUrls: ['./home-sale.component.scss']
 })
 export class HomeSaleComponent implements OnInit {
+
+    isLoading: boolean = false;
+    promotions: IPromotion[] = [];
+    page: number = 1;
+    count: number = 0;
+    error: string = '';
 
     icons = {
         faDumbbell
     }
 
-  constructor() { }
+    constructor(
+        private promotionService: PromotionService,
+    ) { }
 
-  ngOnInit(): void {
-  }
+    ngOnInit(): void {
+
+        this.promotionService.getThreeLatest().subscribe({
+            next: promotions => {
+                this.promotions = promotions;
+                this.isLoading = false;
+            },
+            error: err => {
+                this.isLoading = false;
+                this.error = err.error.message || 'Something went wrong. Please try again later.'
+            }
+        })
+    }
+
+    onCloseNot(): void {
+        this.error = '';
+    }
 
 }
