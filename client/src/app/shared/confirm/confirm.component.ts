9334b4d85d7f32d3561337a9d350a89580d7cdc3
Kuman Stoykov <kumanstoykov@gmail.com>
2022-12-13 23:34:01 +0100

    Create confirm modal

---
+++ client/src/app/shared/confirm/confirm.component.ts
@@ -0,0 +1,103 @@
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { Store } from '@ngrx/store';
+import { Observable } from 'rxjs';
+
+import * as authActions from '../../+store/authStore/actions';
+import * as authSelectors from '../../+store/authStore/selector';
+
+import { clear_message } from 'src/app/+store/authStore/actions';
+import { BenchService } from 'src/app/bench/bench.service';
+import { BikeService } from 'src/app/bike/bike.service';
+import { DumbbellService } from 'src/app/dumbbell/dumbbell.service';
+import { RackService } from 'src/app/rack/rack.service';
+import { TreadmillService } from 'src/app/treadmill/treadmill.service';
+
+@Component({
+    selector: 'app-confirm',
+    templateUrl: './confirm.component.html',
+    styleUrls: ['./confirm.component.scss']
+})
+export class ConfirmComponent implements OnInit {
+
+    message$: Observable<authActions.messageProps | null> = this.store.select(authSelectors.selectMessage);
+
+    isLoading: boolean = false;
+    productType: string = '';
+    productId: string = '';
+
+    deleteRequests: {
+        treadmill: TreadmillService,
+        bike: BikeService,
+        bench: BenchService,
+        dumbbell: DumbbellService,
+        rack: RackService,
+    }
+
+
+
+    constructor(
+        private store: Store,
+        private router: Router,
+        private treadmillService: TreadmillService,
+        private bikeService: BikeService,
+        private rackService: RackService,
+        private benchService: BenchService,
+        private dumbbellService: DumbbellService,
+
+    ) {
+        this.deleteRequests = {
+            'treadmill': this.treadmillService,
+            'bike': this.bikeService,
+            'bench': this.benchService,
+            'dumbbell': this.dumbbellService,
+            'rack': this.rackService,
+        }
+    }
+
+    ngOnInit(): void {
+        this.message$.subscribe({
+            next: message => {
+                this.productType = message?.product!;
+                this.productId = message?._id!;
+            }
+        });
+
+    }
+
+    cancelHandler(): void {
+        this.store.dispatch(clear_message());
+
+    }
+    deleteHandler(): void {
+        this.isLoading = true;
+
+        if (this.productType == 'treadmill'
+            || this.productType == 'bike'
+            || this.productType == 'bench'
+            || this.productType == 'rack'
+            || this.productType == 'dumbbell'
+        ) {
+            console.log(this.productId)
+            this.deleteRequests[this.productType].delete(this.productId).subscribe({
+                next: () => {
+                    this.isLoading = false;
+                    this.router.navigate(['/admin'])
+                    this.store.dispatch(clear_message());
+                },
+                error: err => {
+                    this.isLoading = false;
+                    this.store.dispatch(clear_message());
+                    console.log(err.error.message);
+                }
+            })
+        }
+
+
+
+
+
+
+    }
+
+}
