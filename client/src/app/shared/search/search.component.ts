9746b8f1d3679f3bd83ca5ae63c44a4d1f24dad5
Kuman Stoykov <kumanstoykov@gmail.com>
2022-12-14 20:44:17 +0100

    Added check for promotion

--- client/src/app/shared/search/search.component.ts
+++ client/src/app/shared/search/search.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-search',
@@ -7,9 +8,15 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchComponent implements OnInit {
 
-  constructor() { }
+  isPromotion: boolean = false;
+
+  constructor(
+    private router: Router
+  ) { }
 
   ngOnInit(): void {
+    this.isPromotion = this.router.url.split('?')[0] === '/promotion';
+
   }
 
 }
