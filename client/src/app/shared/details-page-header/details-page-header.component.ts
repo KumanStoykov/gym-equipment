2158e7fbf7902ece0de6327aa7c124f88bc60537
Kuman Stoykov <kumanstoykov@gmail.com>
2022-12-14 20:26:34 +0100

    Added call to delete confirm

--- client/src/app/shared/details-page-header/details-page-header.component.ts
+++ client/src/app/shared/details-page-header/details-page-header.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { IUser } from 'src/app/shared/interfaces';
 import { IAuthState } from 'src/app/+store/authStore/reducers';
@@ -8,6 +9,7 @@ import * as authSelectors from '../../+store/authStore/selector';
 import { faHeart, faCartPlus, faCheck } from '@fortawesome/free-solid-svg-icons';
 import { faHeart as faHeartRegular } from '@fortawesome/free-regular-svg-icons';
 import { Store } from '@ngrx/store';
+import { add_message } from '../../+store/authStore/actions';
 
 
 @Component({
@@ -21,6 +23,8 @@ export class DetailsPageHeaderComponent implements OnInit {
 
     hasPromo: boolean = false;
     isHover: boolean = false;
+    productType: any = '';
+    conIsOpen: boolean = false;
 
     icons = {
         faHeart,
@@ -33,14 +37,28 @@ export class DetailsPageHeaderComponent implements OnInit {
 
     constructor(
         private store: Store<IAuthState>,
+        private router: Router,
     ) { }
 
     ngOnInit(): void {
         this.hasPromo = this.product.promoPrice > 0;
+        this.productType = this.router.url.split('/')[1] === 'strength'
+            ? this.router.url.split('/')[2]
+            : this.router.url.split('/')[1];
     }
 
     hoverHandle(over: boolean): void {
 
         this.isHover = over;
+    }
+
+    deleteConfirm(): void {
+        this.conIsOpen = this.conIsOpen
+        this.store.dispatch(add_message({
+            text: '',
+            typeMsg: 'delete',
+            product: this.productType,
+            _id: this.product._id
+        }));
     }
 }
