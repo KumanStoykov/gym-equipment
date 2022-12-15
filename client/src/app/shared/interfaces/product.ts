bcc3f95217ef33178f11c83afb87ef9b10758b90
Kuman Stoykov <kumanstoykov@gmail.com>
2022-12-15 23:21:23 +0100

    Create product interface

---
+++ client/src/app/shared/interfaces/product.ts
@@ -0,0 +1,9 @@
+export interface IProduct {
+    _id: string;
+    brand: string;
+    images: string[];
+    price: number;
+    promoPrice: number;
+    productType: string;
+    urlPrefix: string;
+}
