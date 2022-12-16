68105b709f63d7d66040b3b615ce1639b43453ee
Kuman Stoykov <kumanstoykov@gmail.com>
2022-12-16 23:49:09 +0100

    Changed delay from 1000 to 500ms

--- client/src/app/shared/validators/email.ts
+++ client/src/app/shared/validators/email.ts
@@ -6,7 +6,7 @@ import { delay, map } from 'rxjs/operators';
 export function emailValidator(): AsyncValidatorFn {
     return (control: AbstractControl): Observable<ValidationErrors | null> => {
         return of({}).pipe(
-            delay(1000),
+            delay(500),
             map(() => {
                 const isMatch = /[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?/.test(control.value);
                 return isMatch ? null : { invalidEmail: true }
--- client/src/app/shared/validators/minLength.ts
+++ client/src/app/shared/validators/minLength.ts
@@ -6,7 +6,7 @@ import { delay, map } from 'rxjs/operators';
 export function minLengthValidator(numLength: number): AsyncValidatorFn {
     return (control: AbstractControl): Observable<ValidationErrors | null> => {
         return of({}).pipe(
-            delay(1000),
+            delay(500),
             map(() => {
                 const valueLength =  control.value.length;
                 return valueLength >= numLength ? null : { minLength: true }
