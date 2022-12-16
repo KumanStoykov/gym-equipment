290a4f24b1d34d7c901cd2e9d489bf6f257518ce
Kuman Stoykov <kumanstoykov@gmail.com>
2022-12-16 23:50:48 +0100

    Added second nav

--- client/src/app/core/header/header.component.ts
+++ client/src/app/core/header/header.component.ts
@@ -2,12 +2,16 @@ import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { Observable } from 'rxjs';
 import { Store } from '@ngrx/store';
+import { faCartShopping , faArrowRightFromBracket, faUser, faGears } from '@fortawesome/free-solid-svg-icons';
+import { faHeart, faUser as faUseRegular } from '@fortawesome/free-regular-svg-icons';
+
 
 import { AuthService } from 'src/app/auth/auth.service';
-import * as authSelectors from '../../+store/authStore/selector';
-import * as authActions from '../../+store/authStore/actions';
+import * as authSelectors from '../../+store/selectors';
+import * as authActions from '../../+store/actions';
 import { IUser } from 'src/app/shared/interfaces';
-import { IAuthState } from 'src/app/+store/authStore/reducers';
+import { IAuthState } from 'src/app/+store/reducers';
+import { cartProps } from '../../+store/actions';
 
 @Component({
     selector: 'app-header',
@@ -16,11 +20,22 @@ import { IAuthState } from 'src/app/+store/authStore/reducers';
 })
 export class HeaderComponent implements OnInit {
 
+    icons = {
+        faCartShopping,
+        faHeart,
+        faArrowRightFromBracket,
+        faUseRegular,
+        faUser,
+        faGears
+
+    }
+
     menuIsToggle: boolean = false;
     strengthIsToggle: boolean = false;
     accountIsToggle: boolean = false;
 
     user$: Observable<IUser | null> = this.store.select(authSelectors.selectUser);
+    cart$: Observable<cartProps[]> = this.store.select(authSelectors.selectCart);
 
 
     constructor(
@@ -45,10 +60,10 @@ export class HeaderComponent implements OnInit {
     }
 
     dropdownToggle(btnName: string): void {
-
         if (btnName === 'account') {
             this.accountIsToggle = !this.accountIsToggle;
             this.strengthIsToggle = false;
+            this.menuIsToggle = false;
 
         } else {
             this.strengthIsToggle = !this.strengthIsToggle;
