615cef20d0c6b78db9838c6c03098dc89fbb117c
Kuman Stoykov <kumanstoykov@gmail.com>
2022-12-18 01:14:36 +0100

    Added basic test

--- client/src/app/auth/login/login.component.spec.ts
+++ client/src/app/auth/login/login.component.spec.ts
@@ -1,23 +1,46 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Store } from '@ngrx/store';
+import { AuthService } from '../auth.service';
 
 import { LoginComponent } from './login.component';
 
+const testStore = {
+    dispatck() {
+        
+    }
+}
 describe('LoginComponent', () => {
-  let component: LoginComponent;
-  let fixture: ComponentFixture<LoginComponent>;
+    let component: LoginComponent;
+    let fixture: ComponentFixture<LoginComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ LoginComponent ]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [LoginComponent],
+            imports: [
+                ReactiveFormsModule,
+                RouterTestingModule,
+                HttpClientModule
+            ],
+            providers: [
+                AuthService,
+                {
+                    provide: Store,
+                    useValue: testStore
+                }
+            ]
+        })
+            .compileComponents();
+    });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(LoginComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-    fixture = TestBed.createComponent(LoginComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
--- client/src/app/auth/register/register.component.spec.ts
+++ client/src/app/auth/register/register.component.spec.ts
@@ -1,23 +1,46 @@
+import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
+import { RouterTestingModule } from '@angular/router/testing';
+import { Store } from '@ngrx/store';
+import { AuthService } from '../auth.service';
 
 import { RegisterComponent } from './register.component';
 
+const testStore = {
+    dispatck() {
+        
+    }
+}
 describe('RegisterComponent', () => {
-  let component: RegisterComponent;
-  let fixture: ComponentFixture<RegisterComponent>;
+    let component: RegisterComponent;
+    let fixture: ComponentFixture<RegisterComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ RegisterComponent ]
-    })
-    .compileComponents();
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [RegisterComponent],
+            imports: [
+                ReactiveFormsModule,
+                RouterTestingModule,
+                HttpClientModule
+            ],
+            providers: [
+                AuthService,
+                {
+                    provide: Store,
+                    useValue: testStore
+                }
+            ]
+        })
+            .compileComponents();
+    });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(RegisterComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-    fixture = TestBed.createComponent(RegisterComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
