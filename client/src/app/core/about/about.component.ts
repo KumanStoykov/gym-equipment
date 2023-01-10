0c9673e41dc0fde1d7a1470b12aa8a6590984dbb
Kuman Stoykov <kumanstoykov@gmail.com>
2023-01-10 12:07:15 +0100

    Added loader to google map

--- client/src/app/core/about/about.component.ts
+++ client/src/app/core/about/about.component.ts
@@ -1,14 +1,26 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { interval, Subscription } from 'rxjs';
 @Component({
-  selector: 'app-about',
-  templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+    selector: 'app-about',
+    templateUrl: './about.component.html',
+    styleUrls: ['./about.component.scss']
 })
-export class AboutComponent implements OnInit {
+export class AboutComponent implements OnInit, OnDestroy{
     heroTitle: string = 'ABOUT US';
-  constructor() { }
+    isLoading = false;
 
-  ngOnInit(): void {
-  }
+    intervalTime = interval(700);
+    subscription: Subscription = new Subscription();
 
+    constructor() { }
+
+    ngOnInit(): void {
+        this.isLoading = true;
+
+        this.subscription = this.intervalTime.subscribe(() => this.isLoading = false);
+    }
+
+    ngOnDestroy(): void {
+        this.subscription.unsubscribe();
+    }
 }
